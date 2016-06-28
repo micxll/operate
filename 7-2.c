cfc23827c4502a5b27cb5e605f2539aa7f675723
micxll <1150637850@qq.com>
2016-06-28 09:06:05 +0000

    7-2-2.c

--- 7-2.c
+++ 7-2.c
@@ -1,43 +1,41 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/msg.h>
 #include <sys/ipc.h>
-#define SHMKEY 75
-int shmid;
-int *loc;
+#include <sys/msg.h>
+#define MSGKEY 75
+
+int shmid, f1, f2, *loc;
 
 void CLIENT(){
 	int i;
-	shmid = shmget(SHMKEY, 1024, 0777);
-	loc = (int *) shmat(shmid, 0, 0);
+	shmid = shmget(MSGKEY, 1024, 0777);
+	loc = shmat(shmid, NULL, 0);
 	for(i = 9; i >= 0; i--){
 		while(*loc != -1);
 		*loc = i;
-		printf("%d", *loc);
-		printf("(client) sent\n");
+		printf("(client) sent %d\n", *loc);
+		sleep(1);
 	}
 	exit(0);
 }
 
 void SERVER(){
-	int i;
-	shmid = shmget(SHMKEY, 1024, 0777|IPC_CREAT);
-	loc = (int *)shmat(shmid, 0, 0);
+	shmid = shmget(MSGKEY, 1024, 0777|IPC_CREAT);
+	loc = shmat(shmid, NULL, 0);
 	do{
 		*loc = -1;
 		while(*loc == -1);
-		printf("%d", *loc+10);
-		printf("(server) received\n");
+		printf("(server) received %d\n", *loc);
+		sleep(1);
 	}while(*loc);
 	shmctl(shmid, IPC_RMID, 0);
 	exit(0);
 }
 
 int main(){
-	int f1, f2;
-	while((f1=fork()) == -1);
+	while((f1 = fork()) == -1);
 	if(f1 == 0) SERVER();
-	while((f2=fork()) == -1);
+	while((f2 = fork()) == -1);
 	if(f2 == 0) CLIENT();
 	wait(0);
 	wait(0);
